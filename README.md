daf99c810ebd8417d988e6b299d9406eaf8acba7
Sniky83 <sniky83@hotmail.fr>
2018-12-05 20:02:30 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Ici, vous trouverez le code source d'une application en C# permettant de mettre en veille l'ordinateur ou de l'éteindre après un téléchargement.
---
+++ Shutdown/Form2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shutdown
+{
+    public partial class Form2 : Form
+    {
+
+        private NetworkInterface[] nicArr;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            InitializeNetworkInterface();
+        }
+
+        private void InitializeNetworkInterface()
+        {
+            nicArr = NetworkInterface.GetAllNetworkInterfaces();
+            List<string> goodAdapters = new List<string>();
+
+            foreach (NetworkInterface nicnac in nicArr)
+            {
+                if (nicnac.SupportsMulticast && nicnac.GetIPv4Statistics().UnicastPacketsReceived >= 1 && nicnac.OperationalStatus.ToString() == "Up")
+                {
+                    goodAdapters.Add(nicnac.Name);
+                }
+
+            }
+            if (goodAdapters.Count != cmbInterface.Items.Count && goodAdapters.Count != 0)
+            {
+                cmbInterface.Items.Clear();
+                foreach (string gadpt in goodAdapters)
+                {
+                    cmbInterface.Items.Add(gadpt);
+                }
+                cmbInterface.SelectedIndex = 0;
+            }
+            if (goodAdapters.Count == 0) cmbInterface.Items.Clear();
+        }
+    }
+}
