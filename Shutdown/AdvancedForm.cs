0ff301b89a29cffd62f992383ce2d4d99bf2fe05
HERVE Louis <sniky83@hotmail.fr>
2019-05-17 19:28:51 +0200

    Maj

--- Shutdown/AdvancedForm.cs
+++ Shutdown/AdvancedForm.cs
@@ -14,12 +14,24 @@ namespace Shutdown
     public partial class AdvancedForm : Form
     {
         public int SelectedItem;
-        public int[] tab = new int[10];
+        public byte[] tab;
         public AdvancedForm()
         {
             InitializeComponent();
             byte i = 0;
             byte j = 0;
+            byte nb = 0;
+
+            foreach (NetworkInterface netInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (netInterface.OperationalStatus == OperationalStatus.Up)
+                {
+                    nb++;
+                }
+            }
+
+            tab = new byte[nb];
+
             foreach (NetworkInterface netInterface in NetworkInterface.GetAllNetworkInterfaces())
             {
                 cmb_Interface.Items.Add(netInterface.Description);
@@ -30,58 +42,11 @@ namespace Shutdown
                 else
                 {
                     tab[j] = i;
-                    //Console.WriteLine(tab[j]);
                     j++;
                 }
                 i++;
             }
-
-            /*List<NetworkInterface> Interfaces = new List<NetworkInterface>();
-            foreach (var nic in NetworkInterface.GetAllNetworkInterfaces())
-            {
-                Interfaces.Add(nic);
-
-                /*if (nic.OperationalStatus == OperationalStatus.Up)
-                {
-                }
-            }
-
-            foreach (NetworkInterface nic in Interfaces)
-            {
-                if (nic.GetIPProperties().GetIPv4Properties() != null)
-                {
-                    cmb_Interface.Items.Add(nic.NetworkInterfaceType);
-                    Console.WriteLine();
-                }
-            }*/
-            /*NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
-            IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
-
-            Console.WriteLine("IPv4 interface information for {0}.{1}",properties.HostName, properties.DomainName);
-
-
-            foreach (NetworkInterface adapter in nics)
-            {
-                if (adapter.Supports(NetworkInterfaceComponent.IPv4) == false)
-                {
-                    continue;
-                }
-
-                //if (!adapter.Description.Equals(adapter.NetworkInterfaceType, StringComparison.OrdinalIgnoreCase))
-                //{
-                    //continue;
-                //}
-                Console.WriteLine(adapter.Description);
-                IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
-                IPv4InterfaceProperties p = adapterProperties.GetIPv4Properties();
-                if (p == null)
-                {
-                    Console.WriteLine("No information is available for this interface.");
-                    continue;
-                }
-                Console.WriteLine("  Index : {0}", p.Index);
-            }*/
-
+            cmb_Interface.Items.RemoveAt(j-1);
             cmb_Interface.SelectedIndex = 0;
         }
 
@@ -92,12 +57,7 @@ namespace Shutdown
 
         private void CmbInterface_SelectedIndexChanged(object sender, EventArgs e)
         {
-            /*string test = e.ToString();
-            int Index = int.Parse(test);
-            cmb_Interface.SelectedIndex;*/
             SelectedItem = tab[cmb_Interface.SelectedIndex];
-            //Console.WriteLine(SelectedItem);
-            //Console.WriteLine(tab[0]);
         }
     }
 }
