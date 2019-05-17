90f2f8786d4d62f7d669d0fee27d8b39530d7e35
HERVE Louis <sniky83@hotmail.fr>
2019-05-17 16:49:19 +0200

    Maj

--- Shutdown/AdvancedForm.cs
+++ Shutdown/AdvancedForm.cs
@@ -17,11 +17,29 @@ namespace Shutdown
         public AdvancedForm()
         {
             InitializeComponent();
-            foreach (NetworkInterface netInterface in NetworkInterface.GetAllNetworkInterfaces())
+            /*foreach (NetworkInterface netInterface in NetworkInterface.GetAllNetworkInterfaces())
             {
                 cmb_Interface.Items.Add(netInterface.NetworkInterfaceType);
                 cmb_Interface.SelectedIndex = 0;
+            }*/
+
+            List<NetworkInterface> Interfaces = new List<NetworkInterface>();
+            foreach (var nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (nic.OperationalStatus == OperationalStatus.Up)
+                {
+                    Interfaces.Add(nic);
+                }
             }
+
+            foreach (NetworkInterface nic in Interfaces)
+            {
+                if (nic.GetIPProperties().GetIPv4Properties() != null)
+                {
+                    cmb_Interface.Items.Add(nic.NetworkInterfaceType);
+                }
+            }
+            cmb_Interface.SelectedIndex = 0;
         }
 
         private void button1_Click(object sender, EventArgs e)
