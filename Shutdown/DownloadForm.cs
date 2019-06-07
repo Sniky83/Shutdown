e973b716f4a80baf7866cc547bd05e9bb852376d
HERVE Louis <sniky83@hotmail.fr>
2019-06-07 19:20:09 +0200

    maj

--- Shutdown/DownloadForm.cs
+++ Shutdown/DownloadForm.cs
@@ -6,6 +6,7 @@ using System.Net.NetworkInformation;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Management;
 
 namespace Shutdown
 {
@@ -132,8 +133,21 @@ namespace Shutdown
                 if (fAdvanced.cmb_OptSys.SelectedItem.ToString() == "Eteindre")
                 {
                     //On éteint l'ordinateur
-                    C_System_Option optSys = new C_System_Option();
-                    optSys.Shutdown();
+                    ManagementBaseObject mboShutdown = null;
+                    ManagementClass mcWin32 = new ManagementClass("Win32_OperatingSystem");
+                    mcWin32.Get();
+
+                    // You can't shutdown without security privileges
+                    mcWin32.Scope.Options.EnablePrivileges = true;
+                    ManagementBaseObject mboShutdownParams = mcWin32.GetMethodParameters("Win32Shutdown");
+
+                    // Flag 1 means we want to shut down the system. Use "2" to reboot.
+                    mboShutdownParams["Flags"] = "1";
+                    mboShutdownParams["Reserved"] = "0";
+                    foreach (ManagementObject manObj in mcWin32.GetInstances())
+                    {
+                        mboShutdown = manObj.InvokeMethod("Win32Shutdown", mboShutdownParams, null);
+                    }
                 }
                 if (fAdvanced.cmb_OptSys.SelectedItem.ToString() == "Mettre en veille")
                 {
